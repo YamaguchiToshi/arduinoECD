cebee7b0c90044879d0aa321807813203a6868b9
Toshimitsu Yamaguchi <t.yamaguchi@eng.niigata-u.ac.jp>
2013-11-26 01:55:23 +0900

    シグナルの送受信用コードの追加
    ただし，IRremoteでは3番ピンを送信用に使っているのでこれでは動かない．

--- arduinoECD.ino
+++ arduinoECD.ino
@@ -1,7 +1,7 @@
 /**
  * Arduino ECD
  *
- * Version 0.0.1  May, 2013
+ * Version 0.0.2  May, 2013
  * Copyright (c) 2013 Toshimitsu Yamaguchi. All rights reserved.
  * 
  */
@@ -31,12 +31,20 @@
 #define POS4     5
 #define POS5     6
 
-// IR signal
-struct IRsignal {
-  unsigned int maker;
-  unsigned long hex;
-  int bits;
-  unsigned int raw[RAWBUF];
+
+// Storage for the recorded code
+int codeType = -1; // The type of code
+unsigned long codeValue; // The code value if not raw
+unsigned int rawCodes[RAWBUF]; // The durations if raw
+int codeLen; // The length of the code
+int toggle = 0; // The RC5/6 toggle state
+
+struct IRcode {
+  int codeType; // The type of code
+  unsigned long codeValue; // The code value if not raw
+  unsigned int rawCodes[RAWBUF]; // The durations if raw
+  int codeLen; // The length of the code
+  int toggle; // The RC5/6 toggle state
 };
 
 // global variable
@@ -49,12 +57,11 @@ int ctrl_val_old = 0;     // previous value of scanning control button
 IRrecv irrecv(IR_IN);     // IR receiver
 IRsend irsend;            // IR sender
 decode_results results;   // IR decode result
-IRsignal received;
-IRsignal sended;
-IRsignal memory[5];
+
+IRcode myIRcodes[5];
 
 void setup(){
-  //pinMode(IR_IN, INPUT);
+  pinMode(IR_IN, INPUT);
   pinMode(IR_OUT, OUTPUT);
   pinMode(ST_LED, OUTPUT);
 
@@ -66,7 +73,7 @@ void setup(){
   pinMode(POS3, OUTPUT);
   pinMode(POS4, OUTPUT); 
   pinMode(POS5, OUTPUT);
-    
+
   MsTimer2::set(50, btnCheck);
   MsTimer2::start();
   
@@ -99,12 +106,11 @@ void loop(){
       digitalWrite(cPOS, !digitalRead(cPOS));
       delay(100);
     }
-    //SENDSENDSENDSENDSEND
-    //irsend.sendRaw(memory[0].raw, 20, 38);
+    sendCode(1);
     STATE = ST_WAITING; // back to WAITING state
   }
 
-  // indicate chaning to RECIEVE mode 
+  // indicate changing to RECIEVE mode 
   if( STATE == ST_COUNT ){
     for( int i = 0; i < 5; i++ ){
       digitalWrite(cPOS, !digitalRead(cPOS));
@@ -125,30 +131,18 @@ void loop(){
   // receiving IR signal
   if( STATE == ST_RECEIVING ){
     if( irrecv.decode(&results) ){
-      int count = results.rawlen;
-      memory[cPOS-2].maker = (unsigned int)results.decode_type;
-      memory[cPOS-2].hex = (unsigned long)results.value;
-      memory[cPOS-2].bits = (int)results.bits;
-      for(int i = 0; i < count; i++){
-        memory[cPOS-2].raw[i] = (int)results.rawbuf[i];
-      }
-      //dump(&results);
-      irrecv.resume(); // Receive the next value
+      myIRcodes[cPOS-2].toggle = 0;
+      myIRcodes[cPOS-2].codeType = -1;
+      storeCode(&results);
     }
-    //irrecv.decode(&results);
-    //Serial.println("*****");
-    //Serial.println(results.value, HEX);
-    STATE = ST_WAITING; // 受信終了後はWAITINGへ
-    digitalWrite(ST_LED, LOW);
-    //irrecv.resume();
   }  
 }
 
 
 void btnCheck(){
+  //Serial.println(STATE);
   md_val = digitalRead(MD_SW);
   ctrl_val = digitalRead(CTRL_SW);
-  
   // mode change SW  
   if( (md_val == HIGH) && (md_val_old == LOW) ){    
     switch( STATE ){
@@ -190,6 +184,107 @@ void btnCheck(){
   md_val_old = md_val;
   ctrl_val_old = ctrl_val;
 }
+
+
+void storeCode(decode_results *results) {
+  myIRcodes[cPOS-2].codeType = results->decode_type;
+  int count = results->rawlen;
+  if (myIRcodes[cPOS-2].codeType == UNKNOWN) {
+    Serial.println("Received unknown code, saving as raw");
+    myIRcodes[cPOS-2].codeLen = results->rawlen - 1;
+    // To store raw codes:
+    // Drop first value (gap)
+    // Convert from ticks to microseconds
+    // Tweak marks shorter, and spaces longer to cancel out IR receiver distortion
+    for (int i = 1; i <= myIRcodes[cPOS-2].codeLen; i++) {
+      if (i % 2) {
+        // Mark
+        myIRcodes[cPOS-2].rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK - MARK_EXCESS;
+        Serial.print(" m");
+      } else {
+        // Space
+        myIRcodes[cPOS-2].rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK + MARK_EXCESS;
+        Serial.print(" s");
+      }
+      Serial.print(myIRcodes[cPOS-2].rawCodes[i - 1], DEC);
+    }
+    Serial.println("");
+  } else {
+    if (myIRcodes[cPOS-2].codeType == NEC) {
+      Serial.print("Received NEC: ");
+      if (results->value == REPEAT) {
+        // Don't record a NEC repeat value as that's useless.
+        Serial.println("repeat; ignoring.");
+        return;
+      }
+    } else if (myIRcodes[cPOS-2].codeType == SONY) {
+      Serial.print("Received SONY: ");
+    } else if (myIRcodes[cPOS-2].codeType == RC5) {
+      Serial.print("Received RC5: ");
+    } else if (myIRcodes[cPOS-2].codeType == RC6) {
+      Serial.print("Received RC6: ");
+    } else {
+      Serial.print("Unexpected codeType ");
+      Serial.print(myIRcodes[cPOS-2].codeType, DEC);
+      Serial.println("");
+    }
+    Serial.println(results->value, HEX);
+    myIRcodes[cPOS-2].codeValue = results->value;
+    myIRcodes[cPOS-2].codeLen = results->bits;
+  }
+  
+  STATE = ST_WAITING; // 受信終了後はWAITINGへ
+  digitalWrite(ST_LED, LOW);
+
+  // ボタンのチェック用タイマの再開
+  MsTimer2::set(50, btnCheck);
+  MsTimer2::start();
+}
+
+
+void sendCode(int repeat) {
+  if (myIRcodes[cPOS-2].codeType == NEC) {
+    if (repeat) {
+      irsend.sendNEC(REPEAT, myIRcodes[cPOS-2].codeLen);
+      Serial.println("Sent NEC repeat");
+    } else {
+      irsend.sendNEC(myIRcodes[cPOS-2].codeValue, myIRcodes[cPOS-2].codeLen);
+      Serial.print("Sent NEC ");
+      Serial.println(myIRcodes[cPOS-2].codeValue, HEX);
+    }
+  } else if (myIRcodes[cPOS-2].codeType == SONY) {
+    irsend.sendSony(myIRcodes[cPOS-2].codeValue, myIRcodes[cPOS-2].codeLen);
+    Serial.print("Sent Sony ");
+    Serial.println(myIRcodes[cPOS-2].codeValue, HEX);
+  } else if (myIRcodes[cPOS-2].codeType == RC5 || myIRcodes[cPOS-2].codeType == RC6) {
+    if (!repeat) {
+      // Flip the toggle bit for a new button press
+      myIRcodes[cPOS-2].toggle = 1 - myIRcodes[cPOS-2].toggle;
+    }
+    // Put the toggle bit into the code to send
+    myIRcodes[cPOS-2].codeValue = myIRcodes[cPOS-2].codeValue & ~(1 << (myIRcodes[cPOS-2].codeLen - 1));
+    myIRcodes[cPOS-2].codeValue = myIRcodes[cPOS-2].codeValue | (myIRcodes[cPOS-2].toggle << (myIRcodes[cPOS-2].codeLen - 1));
+    if (myIRcodes[cPOS-2].codeType == RC5) {
+      Serial.print("Sent RC5 ");
+      Serial.println(myIRcodes[cPOS-2].codeValue, HEX);
+      irsend.sendRC5(myIRcodes[cPOS-2].codeValue, myIRcodes[cPOS-2].codeLen);
+    } else {
+      irsend.sendRC6(myIRcodes[cPOS-2].codeValue, myIRcodes[cPOS-2].codeLen);
+      Serial.print("Sent RC6 ");
+      Serial.println(myIRcodes[cPOS-2].codeValue, HEX);
+    }
+  } else if (myIRcodes[cPOS-2].codeType == UNKNOWN /* i.e. raw */) {
+    // Assume 38 KHz
+    irsend.sendRaw(myIRcodes[cPOS-2].rawCodes, myIRcodes[cPOS-2].codeLen, 38);
+    Serial.println("Sent raw");
+  }
+  
+  // ボタンのチェック用タイマの再開
+  MsTimer2::set(50, btnCheck);
+  MsTimer2::start();
+}
+
+
 
 
 
