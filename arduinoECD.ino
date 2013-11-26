7b23b666afe62379d406e6d19820f3b46b2f06c0
Toshimitsu Yamaguchi <t.yamaguchi@eng.niigata-u.ac.jp>
2013-11-26 17:19:57 +0900

    ピンの変更
    IRremoteの仕様に合わせるため赤外線LEDを3番pinに変更．それに伴いスキャン用のLEDのピンを変更した．

--- arduinoECD.ino
+++ arduinoECD.ino
@@ -1,7 +1,7 @@
 /**
  * Arduino ECD
  *
- * Version 0.0.2  May, 2013
+ * Version 0.0.3  May, 2013
  * Copyright (c) 2013 Toshimitsu Yamaguchi. All rights reserved.
  * 
  */
@@ -19,18 +19,14 @@
 #define ST_COUNT      6
 
 #define IR_IN   12
-#define IR_OUT  11
+//#define IR_OUT  11
 #define ST_LED  10
 
 #define MD_SW    7
 #define CTRL_SW  8
 
-#define POS1     2
-#define POS2     3
-#define POS3     4
-#define POS4     5
-#define POS5     6
-
+// スキャン用LEDのピン
+int pospin[5] = {2, 11, 4, 5, 6};
 
 // Storage for the recorded code
 int codeType = -1; // The type of code
@@ -62,17 +58,15 @@ IRcode myIRcodes[5];
 
 void setup(){
   pinMode(IR_IN, INPUT);
-  pinMode(IR_OUT, OUTPUT);
+//  pinMode(IR_OUT, OUTPUT);
   pinMode(ST_LED, OUTPUT);
 
   pinMode(MD_SW, INPUT);
   pinMode(CTRL_SW, INPUT);
 
-  pinMode(POS1, OUTPUT);
-  pinMode(POS2, OUTPUT);
-  pinMode(POS3, OUTPUT);
-  pinMode(POS4, OUTPUT); 
-  pinMode(POS5, OUTPUT);
+  for( int i = 0; i < 5; ++i ){
+    pinMode(pospin[i], OUTPUT);
+  }
 
   MsTimer2::set(50, btnCheck);
   MsTimer2::start();
@@ -85,6 +79,20 @@ void setup(){
 void loop(){
   // scanning
   if( STATE == ST_SCANNING || STATE == ST_LSCANNING ){
+    for( int i = 0; i < 5; ++i ){
+      digitalWrite(pospin[i], HIGH);
+      if( i == 0 ){
+        digitalWrite(pospin[4], LOW);
+      } else {
+        digitalWrite(pospin[i-1], LOW);
+      }
+      cPOS = pospin[i];
+      delay(800);
+      if( STATE == ST_SENDING || STATE == ST_COUNT ){
+        break;
+      }
+    }
+    /*
     for( cPOS = POS1; cPOS <= POS5; cPOS++ ){
       digitalWrite(cPOS, HIGH);
       if( cPOS > POS1 ){
@@ -96,7 +104,8 @@ void loop(){
       if( STATE == ST_SENDING || STATE == ST_COUNT ){
         break;
       }
-    }
+    }*/
+
   }
 
   // send IR signal
@@ -116,14 +125,15 @@ void loop(){
       digitalWrite(cPOS, !digitalRead(cPOS));
       delay(100);
     }
-    for( int i = POS5; i >= POS1; i-- ){
-      digitalWrite(i, HIGH);
+    for( int i = 0; i < 5; i++ ){
+      digitalWrite(pospin[i], HIGH);
       delay(300);
     }
-    for( int i = POS1; i <= POS5; i++ ){
-      digitalWrite(i, LOW);
-      delay(1000);
+    for( int i = 4; i >= 0; i-- ){
+      digitalWrite(pospin[i], LOW);
+      delay(400);
     }
+
     irrecv.enableIRIn();    // IR receiver on
     STATE = ST_RECEIVING;   // change state 
   }
@@ -131,8 +141,8 @@ void loop(){
   // receiving IR signal
   if( STATE == ST_RECEIVING ){
     if( irrecv.decode(&results) ){
-      myIRcodes[cPOS-2].toggle = 0;
-      myIRcodes[cPOS-2].codeType = -1;
+      myIRcodes[pin2pos(cPOS)].toggle = 0;
+      myIRcodes[pin2pos(cPOS)].codeType = -1;
       storeCode(&results);
     }
   }  
@@ -186,51 +196,62 @@ void btnCheck(){
 }
 
 
+int pin2pos(int pinnum){
+  for( int i = 0; i < 5; ++i ){
+    if( pospin[i] == pinnum ){
+      return i;
+    }
+  }
+  return 100;
+}
+
+
 void storeCode(decode_results *results) {
-  myIRcodes[cPOS-2].codeType = results->decode_type;
+  int storeID = pin2pos(cPOS);
+  myIRcodes[storeID].codeType = results->decode_type;
   int count = results->rawlen;
-  if (myIRcodes[cPOS-2].codeType == UNKNOWN) {
+  if (myIRcodes[storeID].codeType == UNKNOWN) {
     Serial.println("Received unknown code, saving as raw");
-    myIRcodes[cPOS-2].codeLen = results->rawlen - 1;
+    myIRcodes[storeID].codeLen = results->rawlen - 1;
     // To store raw codes:
     // Drop first value (gap)
     // Convert from ticks to microseconds
     // Tweak marks shorter, and spaces longer to cancel out IR receiver distortion
-    for (int i = 1; i <= myIRcodes[cPOS-2].codeLen; i++) {
+    for (int i = 1; i <= myIRcodes[storeID].codeLen; i++) {
       if (i % 2) {
         // Mark
-        myIRcodes[cPOS-2].rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK - MARK_EXCESS;
+        myIRcodes[storeID].rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK - MARK_EXCESS;
         Serial.print(" m");
       } else {
         // Space
-        myIRcodes[cPOS-2].rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK + MARK_EXCESS;
+        myIRcodes[storeID].rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK + MARK_EXCESS;
         Serial.print(" s");
       }
-      Serial.print(myIRcodes[cPOS-2].rawCodes[i - 1], DEC);
+      Serial.print(myIRcodes[storeID].rawCodes[i - 1], DEC);
     }
     Serial.println("");
   } else {
-    if (myIRcodes[cPOS-2].codeType == NEC) {
+    if (myIRcodes[storeID].codeType == NEC) {
       Serial.print("Received NEC: ");
       if (results->value == REPEAT) {
         // Don't record a NEC repeat value as that's useless.
         Serial.println("repeat; ignoring.");
         return;
       }
-    } else if (myIRcodes[cPOS-2].codeType == SONY) {
+    } else if (myIRcodes[storeID].codeType == SONY) {
       Serial.print("Received SONY: ");
-    } else if (myIRcodes[cPOS-2].codeType == RC5) {
+    } else if (myIRcodes[storeID].codeType == RC5) {
       Serial.print("Received RC5: ");
-    } else if (myIRcodes[cPOS-2].codeType == RC6) {
+    } else if (myIRcodes[storeID].codeType == RC6) {
       Serial.print("Received RC6: ");
     } else {
       Serial.print("Unexpected codeType ");
-      Serial.print(myIRcodes[cPOS-2].codeType, DEC);
+      Serial.print(myIRcodes[storeID].codeType, DEC);
       Serial.println("");
     }
     Serial.println(results->value, HEX);
-    myIRcodes[cPOS-2].codeValue = results->value;
-    myIRcodes[cPOS-2].codeLen = results->bits;
+    myIRcodes[storeID].codeValue = results->value;
+    myIRcodes[storeID].codeLen = results->bits;
   }
   
   STATE = ST_WAITING; // 受信終了後はWAITINGへ
@@ -243,39 +264,40 @@ void storeCode(decode_results *results) {
 
 
 void sendCode(int repeat) {
-  if (myIRcodes[cPOS-2].codeType == NEC) {
+  int storeID = pin2pos(cPOS); 
+  if (myIRcodes[storeID].codeType == NEC) {
     if (repeat) {
-      irsend.sendNEC(REPEAT, myIRcodes[cPOS-2].codeLen);
+      irsend.sendNEC(REPEAT, myIRcodes[storeID].codeLen);
       Serial.println("Sent NEC repeat");
     } else {
-      irsend.sendNEC(myIRcodes[cPOS-2].codeValue, myIRcodes[cPOS-2].codeLen);
+      irsend.sendNEC(myIRcodes[storeID].codeValue, myIRcodes[storeID].codeLen);
       Serial.print("Sent NEC ");
-      Serial.println(myIRcodes[cPOS-2].codeValue, HEX);
+      Serial.println(myIRcodes[storeID].codeValue, HEX);
     }
-  } else if (myIRcodes[cPOS-2].codeType == SONY) {
-    irsend.sendSony(myIRcodes[cPOS-2].codeValue, myIRcodes[cPOS-2].codeLen);
+  } else if (myIRcodes[storeID].codeType == SONY) {
+    irsend.sendSony(myIRcodes[storeID].codeValue, myIRcodes[storeID].codeLen);
     Serial.print("Sent Sony ");
-    Serial.println(myIRcodes[cPOS-2].codeValue, HEX);
-  } else if (myIRcodes[cPOS-2].codeType == RC5 || myIRcodes[cPOS-2].codeType == RC6) {
+    Serial.println(myIRcodes[storeID].codeValue, HEX);
+  } else if (myIRcodes[storeID].codeType == RC5 || myIRcodes[storeID].codeType == RC6) {
     if (!repeat) {
       // Flip the toggle bit for a new button press
-      myIRcodes[cPOS-2].toggle = 1 - myIRcodes[cPOS-2].toggle;
+      myIRcodes[storeID].toggle = 1 - myIRcodes[storeID].toggle;
     }
     // Put the toggle bit into the code to send
-    myIRcodes[cPOS-2].codeValue = myIRcodes[cPOS-2].codeValue & ~(1 << (myIRcodes[cPOS-2].codeLen - 1));
-    myIRcodes[cPOS-2].codeValue = myIRcodes[cPOS-2].codeValue | (myIRcodes[cPOS-2].toggle << (myIRcodes[cPOS-2].codeLen - 1));
-    if (myIRcodes[cPOS-2].codeType == RC5) {
+    myIRcodes[storeID].codeValue = myIRcodes[storeID].codeValue & ~(1 << (myIRcodes[storeID].codeLen - 1));
+    myIRcodes[storeID].codeValue = myIRcodes[storeID].codeValue | (myIRcodes[storeID].toggle << (myIRcodes[storeID].codeLen - 1));
+    if (myIRcodes[storeID].codeType == RC5) {
       Serial.print("Sent RC5 ");
-      Serial.println(myIRcodes[cPOS-2].codeValue, HEX);
-      irsend.sendRC5(myIRcodes[cPOS-2].codeValue, myIRcodes[cPOS-2].codeLen);
+      Serial.println(myIRcodes[storeID].codeValue, HEX);
+      irsend.sendRC5(myIRcodes[storeID].codeValue, myIRcodes[storeID].codeLen);
     } else {
-      irsend.sendRC6(myIRcodes[cPOS-2].codeValue, myIRcodes[cPOS-2].codeLen);
+      irsend.sendRC6(myIRcodes[storeID].codeValue, myIRcodes[storeID].codeLen);
       Serial.print("Sent RC6 ");
-      Serial.println(myIRcodes[cPOS-2].codeValue, HEX);
+      Serial.println(myIRcodes[storeID].codeValue, HEX);
     }
-  } else if (myIRcodes[cPOS-2].codeType == UNKNOWN /* i.e. raw */) {
+  } else if (myIRcodes[storeID].codeType == UNKNOWN /* i.e. raw */) {
     // Assume 38 KHz
-    irsend.sendRaw(myIRcodes[cPOS-2].rawCodes, myIRcodes[cPOS-2].codeLen, 38);
+    irsend.sendRaw(myIRcodes[storeID].rawCodes, myIRcodes[storeID].codeLen, 38);
     Serial.println("Sent raw");
   }
   
@@ -284,64 +306,3 @@ void sendCode(int repeat) {
   MsTimer2::start();
 }
 
-
-
-
-
-
-
-
-void dump(decode_results *results) {
-
-  int count = results->rawlen;
-
-  //memory[cPOS-2].maker = (unsigned int)results->decode_type;
-  //memory[cPOS-2].hex = (unsigned long)results->value;
-  //memory[cPOS-2].bits = (int)results->bits;
-  //for(int i = 0; i < count; i++){
-  //  memory[cPOS-2].raw[i] = (int)results->rawbuf[i];
-  //}
-
-  if (results->decode_type == UNKNOWN) {
-    Serial.print("Unknown encoding: ");
-  } 
-  else if (results->decode_type == NEC) {
-    Serial.print("Decoded NEC: ");
-  } 
-  else if (results->decode_type == SONY) {
-    Serial.print("Decoded SONY: ");
-  } 
-  else if (results->decode_type == RC5) {
-    Serial.print("Decoded RC5: ");
-  } 
-  else if (results->decode_type == RC6) {
-    Serial.print("Decoded RC6: ");
-  }
-  else if (results->decode_type == PANASONIC) {	
-    Serial.print("Decoded PANASONIC - Address: ");
-    Serial.print(results->panasonicAddress,HEX);
-    Serial.print(" Value: ");
-  }
-  else if (results->decode_type == JVC) {
-     Serial.print("Decoded JVC: ");
-  }
-  Serial.print(results->value, HEX);
-  Serial.print(" (");
-  Serial.print(results->bits, DEC);
-  Serial.println(" bits)");
-  Serial.print("Raw (");
-  Serial.print(count, DEC);
-  Serial.print("): ");
-
-  for (int i = 0; i < count; i++) {
-    if ((i % 2) == 1) {
-      Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
-    } 
-    else {
-      Serial.print(-(int)results->rawbuf[i]*USECPERTICK, DEC);
-    }
-    Serial.print(" ");
-  }
-  Serial.println("");
-
-}
